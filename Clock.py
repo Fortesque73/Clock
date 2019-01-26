c5f0074941d4f602e4c20e4d486bd01277c9fb82
hielo <juanfgarciacas@gmail.com>
2019-01-26 23:40:40 +0100

    Update Clock.py

--- Clock.py
+++ Clock.py
@@ -3,33 +3,36 @@ import time
 
 class Clock:
     #Init clock with a previous set alarm
-    def __init__(self, datetime):
-        self.alarm = datetime
+    def __init__(self, alarmTime):
+        self.currentTime =  datetime.now()
+        self.cuerrentTime = self.currentTime.strftime("%d-%m-%Y %H:%M:%S")
+        self.alarm = alarmTime
         self.alarm = self.alarm.strftime("%d-%m-%Y %H:%M:%S")
 
-    #returns current time in string
-    def getCurrentTime(self):
-        return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    #updates current time in string
+    def updateCurrentTime(self):
+        self.currentTime = datetime.now()
+        self.cuerrentTime = self.currentTime.strftime("%d-%m-%Y %H:%M:%S")
 
     #prints current time in string format every second
     def show(self):
-        print (self.getCurrentTime(), type(self.getCurrentTime), end="\r")
-        time.sleep(1)
+        self.updateCurrentTime()
+        print (self.currrentTime end="\r")
+
 
     #If current time and alarm time are equal return true
     def alarmSound(self):
-        if self.getCurrentTime == self.alarm :
+        if self.currentTime is self.alarm :
             return True
 
-    def showAlarm(self):
-        print (self.alarm)
-
-
 alarm = datetime(2019, 1, 26, 18, 28, 15, 00)
-clock = Clock(alarm)
+currentTime = datetime.now()
+clock = Clock(alarm, currentTime)
 exit = False
 while exit is False:
     clock.show()
     if clock.alarmSound() is True:
         exit = True
+        
+    time.sleep(1)
 print("Pasa algo")
