d76f15b2bd6493cb54dbd764f42bf89c81344b91
hielo <juanfgarciacas@gmail.com>
2019-01-26 18:12:01 +0100

    Updated Clock.py

--- Clock.py
+++ Clock.py
@@ -2,14 +2,27 @@ from datetime import datetime
 import time
 
 class Clock:
+    #Init clock with a previous set alarm
+    def __init__(self, datetime):
+        self.alarm = datetime
 
+    #returns current time in string
     def getCurrentTime(self):
         return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
+    #prints current time in string format every second
     def show(self):
         while True :
             print (self.getCurrentTime(), end="\r")
             time.sleep(1)
 
-x = Clock()
-x.show()
+    def alarm(self):
+        self.alarm = self.alarm.strftime("%d-%m-%Y %H:%M:%S")
+
+    def showAlarm(self):
+        print (self.alarm)
+
+
+alarm = datetime(2019, 1, 26, 18, 5, 00, 00)
+x = Clock(alarm)
+x.showAlarm()
