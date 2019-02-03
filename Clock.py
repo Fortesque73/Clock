f3b4500e52075c616d8f9c8d6903122522535b17
Fortesque73 <gba007@hotmail.es>
2019-02-03 02:08:28 +0100

    created alarm object
    now clocks have a list of alarms. The function alarmSound now its called checkAlarms, and it returns an int instead of a boolean. Plans of creating some booleans for the Alarm object to determine wich attributes of the datetime object are meant to take in account (ms are never taken in account).

--- Clock.py
+++ Clock.py
@@ -1,15 +1,42 @@
 from datetime import datetime
 import time
 
+class Alarm:
+    def __init__(self, alarmTime, ringtone):
+        self.time=alarmTime
+        self.ringtone=ringtone
+        self.timeNoMilis = alarmTime.strftime("%d-%m-%Y %H:%M:%S") #time in string
 
+    def play(self):
+        #here some code that makes the alarm sound
+        pass
 
 class Clock:
     #alarmTime=datetime, currentTime=datetime
-    def __init__(self, alarmTime, currentTime):
+    def __init__(self, currentTime):
         self.currentTime =  datetime.now()
         self.cuerrentTime = self.currentTime.strftime("%d-%m-%Y %H:%M:%S")
-        self.alarm = alarmTime
-        self.alarmNoMilis = self.alarm.strftime("%d-%m-%Y %H:%M:%S")
+        self.alarms = []
+        self.numalarms=0
+
+    #set an alarm manually
+    def setalarm(self):
+        hour=raw_input("introduce alarm's hour: ")
+        minute=raw_input("introduce minute: ")
+        addalarm(hour, minute)
+
+    #add the alarm to the list hour=int, minute=int
+    def addalarm(self, hour, minute):
+        curr=datetime.now()
+        hour%=24
+        minute%=60
+        self.alarms.append(Alarm(datetime(curr.year, curr.month, curr.day, hour, minute, 00, 00)))
+        self.numalarms+=1
+
+    #add alarms for debugging, newalarm=Alarm
+    def quickalarm(self, newalarm):
+        self.alarms.append(newalarm)
+        self.numalarms+=1
 
     #updates current time in string
     def updateCurrentTime(self):
@@ -20,25 +47,39 @@ class Clock:
     def show(self):
         self.updateCurrentTime()
         print (self.currentTimeNoMilis, end="\r")
-        
 
+    #If current time and any alarm time are equal return true
+    def checkAlarms(self):
+        i=0
+        while (i<self.numalarms) :
+            if self.currentTimeNoMilis == self.alarms[i].timeNoMilis:
+                return i
+            i+=1
+        return -1
 
-    #If current time and alarm time are equal return true
-    def alarmSound(self):
-        if self.currentTimeNoMilis == self.alarmNoMilis:
-            return True
-        else:
-            return False
-
+#First create a clock instance
 currentTime = datetime.now()
-alarm = datetime(2019, 1, 28, 16, 47, 32, 00)
-clock = Clock(alarm, currentTime)
+clock=Clock(currentTime)
+#create some alarms
+alarmtime = datetime(2019, 2, 3, 1, 56, 20, 00)
+alarm=Alarm(alarmtime, "")
+
+#add the alarms to the clock
+clock.quickalarm(alarm)
+
+i=0
+while(i<clock.numalarms):
+    print(clock.alarms[i].timeNoMilis)
+    i+=1
+
 exit = False
-print ("alarm "+clock.alarmNoMilis)
+print ("alarm "+clock.alarms[0].timeNoMilis)
 while exit is False:
     clock.show()
-    if clock.alarmSound() == True:
+    sound=clock.checkAlarms()#sound=int with the alarms' id that will sound
+    print(sound)
+    if sound >= 0:
         exit = True
-        
+        print('sonido pa tu culo')
+        print(sound)
     time.sleep(1)
-print("Pasa algo")
